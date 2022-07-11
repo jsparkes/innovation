0328e886e59eabc066735d89fabd2d0bb2b60c46
frankk74 <frankk74@gmail.com>
2022-07-10 23:21:35 -0400

    Rebuild of app verion in \bin has VERBOSE turned on the one \publish has it turned off

--- main.vb
+++ main.vb
@@ -5,7 +5,7 @@ Imports Microsoft.VisualBasic.PowerPacks
 Imports System.Collections.Generic
 'UPGRADE_NOTE: Main was upgraded to Main_Renamed. Click for more: 'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="A9E4979A-37FA-4718-9994-97DD76ED70A7"'
 
-#Const VERBOSE = False ' FK adding debugging Frame work via c compiler like if defs should be DEBUG but not sure about interference
+#Const VERBOSE = True ' FK adding debugging Frame work via c compiler like if defs should be DEBUG but not sure about interference
 
 Friend Class Main_Renamed
     Inherits System.Windows.Forms.Form
